7eb60941111784ba81acffb36845e834bb90674c
Deeraj Gurram <djgurram@gmail.com>
2025-04-06 23:27:33 -0700

    Added error and exception handling

--- backend/fastapi_app/main.py
+++ backend/fastapi_app/main.py
@@ -4,7 +4,7 @@ FastAPI app for the transcription service
 
 from uuid import UUID
 
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, status
 
 from backend.transcribe.transcribe import transcribe_file
 
@@ -16,10 +16,20 @@ async def transcribe(tenant_id: UUID, file: UploadFile = File(...)):
     """
     Transcribe a file and return the transcription
     """
-    # Get the file content
-    file_content = await file.read()
+    try:
+        # Get the file content
+        file_content = await file.read()
+    except (TimeoutError, ConnectionError) as e:
+        status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        detail = str(e)
+        return {"error": detail, "status_code": status_code}
 
-    # Transcribe the file
-    transcription = await transcribe_file(tenant_id, file_content)
+    try:
+        # Transcribe the file
+        transcription = await transcribe_file(tenant_id, file_content)
+    except ValueError as e:
+        status_code = status.HTTP_400_BAD_REQUEST
+        detail = str(e)
+        return {"error": detail, "status_code": status_code}
 
     return {"transcription": transcription}
